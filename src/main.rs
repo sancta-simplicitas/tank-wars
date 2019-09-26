9137da27c72be8761f36a144653cc7e533a0ba58
hzqd <hzqelf@yeah.net>
2019-09-27 00:54:06 +0800

    change game engine

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,18 @@
-use amethyst::{
-    core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resources, SystemData},
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    utils::application_root_dir,
-};
+extern crate piston_window;
 
-struct MyState;
+use piston_window::*;
 
-impl SimpleState for MyState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
-}
-
-fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
-
-    let app_root = application_root_dir()?;
-
-    let config_dir = app_root.join("config");
-    let display_config_path = config_dir.join("display.ron");
-
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderFlat2D::default()),
-        )?
-        .with_bundle(TransformBundle::new())?;
-
-    let mut game = Application::new("/", MyState, game_data)?;
-    game.run();
-
-    Ok(())
+fn main() {
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true).build().unwrap();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      context.transform,
+                      graphics);
+        });
+    }
 }
