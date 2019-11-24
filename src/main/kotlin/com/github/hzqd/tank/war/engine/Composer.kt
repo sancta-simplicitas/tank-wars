9e02711fac066abcb17c5122777dd00105952dd5
hzqd <hzqdfs@gmail.com>
2019-11-24 15:14:27 +0800

    negative optimized code

--- src/main/kotlin/com/github/hzqd/tank/war/engine/Composer.kt
+++ src/main/kotlin/com/github/hzqd/tank/war/engine/Composer.kt
@@ -2,19 +2,22 @@ package com.github.hzqd.tank.war.engine
 
 import java.io.BufferedInputStream
 import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.Clip
+import javax.sound.sampled.DataLine
 
 object Composer {
-    fun play(soundPath: String) = doPlay(soundPath)
-
-    private fun doPlay(soundPath: String) {
-        AudioSystem.getClip()?.let {
-            BufferedInputStream(javaClass.getResourceAsStream("/${soundPath}")).run {
-                AudioSystem.getAudioInputStream(this)
-            }.run {
-                with(it) {
-                    addLineListener { event -> if (event.framePosition.toInt() == frameLength) close() }
-                    open(this@run)
-                    start()
+    fun play(soundPath: String) {
+        BufferedInputStream(javaClass.getResourceAsStream("/$soundPath")).let {
+            AudioSystem.getAudioInputStream(it)
+        }.run {
+            with(format) {
+                (frameSize * frameLength).toInt().let { size ->
+                    ByteArray(size).let { audio ->
+                        read(audio, 0, size)
+                        (AudioSystem.getLine(DataLine.Info(Clip::class.java, this, size)) as Clip).apply {
+                            open(this@with, audio, 0, size)
+                        }.start()
+                    }
                 }
             }
         }
